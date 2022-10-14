a638ddef7547483a3086e74bc9f4109a68584048
ainuo <ainuo@fanruan.com>
2022-10-14 15:45:52 +0800

    fix: 去除一部分多余的代码

--- src/core/middleware/index.ts
+++ src/core/middleware/index.ts
@@ -12,27 +12,12 @@ export abstract class AbsMiddleware {
 
 export abstract class ModuleMiddlware extends AbsMiddleware {
   readonly __flag: MiddlewareType = 'module'
-  abstract use(
-    request: IncomingMessage,
-    response: ServerResponse,
-    next: () => void
-  ): void
 }
 
 export abstract class ControllerMiddlware extends AbsMiddleware {
   readonly __flag: MiddlewareType = 'controller'
-  abstract use(
-    request: IncomingMessage,
-    response: ServerResponse,
-    next: () => void
-  ): void
 }
 
 export abstract class RouteMiddlware extends AbsMiddleware {
   readonly __flag: MiddlewareType = 'route'
-  abstract use(
-    request: IncomingMessage,
-    response: ServerResponse,
-    next: () => void
-  ): void
 }
